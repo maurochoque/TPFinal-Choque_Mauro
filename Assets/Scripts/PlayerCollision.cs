01817abed747ff5e6385cb77634a8d2eefbc851a
maurochoque <mauro91choque@gmail.com>
2023-11-02 20:45:46 -0300

    se agrego escenas
    se agrego escenas MenuInicial, Perder y cuando se cumple una condicion, se cambia de escena

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,26 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;//
 public class PlayerCollision : MonoBehaviour
 {
   
-
+    public string nextSceneName = "Perdiste";//
     private void OnCollisionEnter2D(Collision2D collision)
 {
     // Maneja colisiones físicas
     if (collision.gameObject.CompareTag("Enemy"))
     {
         Destroy(gameObject);
+        SceneManager.LoadScene(nextSceneName);//
         Debug.Log("Colisión física con un objeto etiquetado como 'Circle'");
         // Realiza las acciones necesarias.
     }
     if (collision.gameObject.CompareTag("ObstaculosPuas"))
     {
         Destroy(gameObject);
+        SceneManager.LoadScene(nextSceneName);//
         Debug.Log("Colisión física con un objeto etiquetado como 'Circle'");
+        
         // Realiza las acciones necesarias.
     }
+    
 }
 
     //AGREGAR COLISIONES CON OTROS ENEMIGOS, INTERFAZ DE INICIO, PANTALLA DE PERDER/GANAR, SISTEMA DE RECOLECCION
